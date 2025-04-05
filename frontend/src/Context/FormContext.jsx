f888abb25d82115c609fc7292a4d1c64864b0e05
amxr21 <ammar211080@gmail.com>
2025-04-05 23:39:24 +0400

    .

--- frontend/src/Context/FormContext.jsx
+++ frontend/src/Context/FormContext.jsx
@@ -4,8 +4,6 @@ import { useState, useRef, createContext } from "react";
 import { useAuthContext } from "../Hooks/useAuthContext";
 
 
-const link = "https://jobfair-1.onrender.com"
-
 
 export const FormContext = createContext();
 
