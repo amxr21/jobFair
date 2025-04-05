c9fc29d18cb57d38cefd363071fba715e8032a89
amxr21 <ammar211080@gmail.com>
2025-04-05 23:30:45 +0400

    update schema structure

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,10 @@ const validator = require("validator")
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    companyName : {
+        type: String,
+        required: true,
+    },
     email : {
         type: String,
         required: true,
@@ -14,11 +18,19 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
+    representitives: {
+        type: String,
+        required: true,
+    },
+    fields: {
+        type: String,
+        required: true,
+    },
 })
 
-userSchema.statics.signup = async function(email, password) {
+userSchema.statics.signup = async function(email, password, fields, representitives, companyName ) {
     
-    if(!email || !password){
+    if(!email || !password || !representitives || !fields || !companyName){
         throw Error("All fields must be filled");
     }
     if(!validator.isEmail(email)){
@@ -36,7 +48,7 @@ userSchema.statics.signup = async function(email, password) {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
 
-    const user = await this.create({email, password: hash})
+    const user = await this.create({email, password: hash, fields, representitives, companyName})
 
     return user;
 }
