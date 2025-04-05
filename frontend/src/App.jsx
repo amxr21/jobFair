a690e3a4411ac2ac3f95d2c026fed90d2abc938e
amxr21 <ammar211080@gmail.com>
2025-04-05 23:39:01 +0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,38 @@
 import "./style.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
-import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
-import { NavBar } from "./components/ControlDashboard/index";
+import { NavBar, MainBanner } from "./components/index";
+
+import Managers from "./pages/Managers";
+import Statistics from "./pages/Statistics";
 
 import { useAuthContext } from "./Hooks/useAuthContext";
 
-import { ApplicationForm } from "./components/ApplicationForm";
 
-import ApplicantPage from "./pages/ApplicantPage";
-import Intro from "./pages/Intro";
+import { useState } from "react";
 
-const link = "https://jobfair-91pd.onrender.com"
+const link = "https://jobfairform-backend.onrender.com"
 
 
 function App() {
   const { user } = useAuthContext();
 
-
+  const [list, setList] = useState([])
 
   return (
 
-    <div className="App relative px-10 py-20 md:px-40 md:py-10">
+    <div className="App grid grid-cols-12 relative gap-x-12 px-10 py-20 h-[100vh] px-10 md:py-8">
       {/* <Intro /> */}
-      <NavBar/>
       <BrowserRouter>
+      <NavBar/>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/applicationForm" element={<ApplicationForm />} />
+          <Route path="/" element={<MainBanner link={link}/>} />
+          <Route path="/managers" element={<Managers link={link} />} />
+          <Route path="/statistics" element={<Statistics link={link} />} />
           <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
           <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
-          <Route path="/applicant" element={<ApplicantPage />} />
         </Routes>
       </BrowserRouter>
     </div>
