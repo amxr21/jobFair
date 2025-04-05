b00de2c07f32ba1a774255b5be60785766a1d802
amxr21 <ammar211080@gmail.com>
2025-04-05 23:33:55 +0400

    delete unrelated pages & components

--- frontend/src/components/ApplicantPage/index.js
+++
@@ -1,7 +0,0 @@
-export { default as CheckId } from "../ApplicantPage/CheckId";
-export { default as QrScanner } from "./QrScanner";
-export { default as IdProvider } from "../../Context/IdContext";
-export { default as IdContext } from "../../Context/IdContext";
-
-
-// import CheckId from "../ApplicantPage/CheckId"
--- frontend/src/components/ApplicationForm/FormHeader.jsx
+++
@@ -1,7 +0,0 @@
-const FormHeader = ({header}) => {
-    return (
-        <h1 className="text-3xl md:mb-8 underline font-bold">{header}</h1>
-    )
-}
-
-export default FormHeader;
