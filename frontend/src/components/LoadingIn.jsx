73b1699202aa33d2485a62f13bf213732c454b7e
amxr21 <ammar211080@gmail.com>
2025-04-12 18:23:48 +0400

    update login/signup layout and function

---
+++ frontend/src/components/LoadingIn.jsx
@@ -0,0 +1,17 @@
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+
+
+const LoadingIn = () => {
+    return (
+        <div className="absolute top-0 left-0 bg-white w-full h-full p-10 flex flex-col gap-y-16 items-center justify-center text-5xl font-bold">
+            <h2>Siginin you In...</h2>
+            <Box sx={{ width: '16rem' }}>
+                <LinearProgress color='success' />
+            </Box>
+        </div>
+    )
+}
+
+
+export default LoadingIn
