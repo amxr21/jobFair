61f766128c9e5d7e5a21ca902cb4ef757a59ee5e
amxr21 <ammar211080@gmail.com>
2025-04-14 12:51:16 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,9 @@ dotenv.config();
 //   });
 
 const corsOptions = {
-  origin: "https://job-fair-lilac.vercel.app", // or '*' if open access is OK
+  // origin: "https://job-fair-lilac.vercel.app", // or '*' if open access is OK
   // origin: "https://job-fair-cd5j.onrender.com", // or '*' if open access is OK
-  // origin: "http://localhost:3000", // or '*' if open access is OK
+  origin: "http://localhost:3000", // or '*' if open access is OK
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
 };
