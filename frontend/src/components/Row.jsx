9d3520dfedd631002af2692fd6b4d1b07ce4f2db
amxr21 <ammar211080@gmail.com>
2025-04-18 17:25:27 +0400

    add uni id validation func

--- frontend/src/components/Row.jsx
+++ frontend/src/components/Row.jsx
@@ -84,7 +84,7 @@ const Row = ({number, name, ticketId, uniId, email, phoneNumber, studyLevel, maj
                     
                 </h2>
                 <h2 className="flex">{name}</h2>
-                <h2 className="flex">{uniId}</h2>
+                <h2 className="flex">{uniId == "" || uniId?.length != 8 || uniId == 18000000 ? '00000000' : uniId}</h2>
                 <h2 className="flex">{nationality}</h2>
                 {/* <h2 className="flex">{age}</h2> */}
                 <h2 className="flex">{gpa == 0 || gpa == NaN  ? 'XX' : parseFloat(gpa)?.toFixed(2)}</h2>
@@ -108,7 +108,7 @@ const Row = ({number, name, ticketId, uniId, email, phoneNumber, studyLevel, maj
                     
                     
                     
-                    <div ref={expandApplicantDiv} className={`expandDetails parent bg-white shadow-2xl rounded-xl px-8 py-14 w-80 md:w-[58em] md:max-w-[196em] h-[50rem] overflow-y-scroll md:overflow-y-auto md:h-fit md:max-h-[42rem] fixed top-[48%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${isVisible ? 'opacity-100 z-[99999]' : 'opacity-0 -z-[9999]'}`}>
+                    <div ref={expandApplicantDiv} className={`expandDetails parent bg-white shadow-2xl rounded-xl px-8 py-14 w-80 md:w-[58em] md:max-w-[196em] h-[50rem] overflow-y-scroll md:overflow-y-auto md:h-fit md:max-h-[38rem] fixed top-[48%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${isVisible ? 'opacity-100 z-[99999]' : 'opacity-0 -z-[9999]'}`}>
                         <div className="card-info flex flex-col md:flex-row gap-x-4 md:h-11/12">
                             <Brief>
                                 <BriefInfo
