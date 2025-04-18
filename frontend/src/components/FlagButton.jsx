a25283421ada3e289e54330488dacdb370733b7b
amxr21 <ammar211080@gmail.com>
2025-04-18 17:24:24 +0400

    Create FlagButton.jsx

---
+++ frontend/src/components/FlagButton.jsx
@@ -0,0 +1,13 @@
+const FlagButton = ({btnRef, handleClick}) => {
+    return (
+        <button className="border border-gray-300 opacity-50 rounded-xl w-10 h-10 bg-[#F3F6FF] flex items-center justify-center" ref={btnRef} onClick={handleClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5" />
+            </svg>
+        </button>
+    )
+}
+
+
+
+export default FlagButton
