bb4519ce82f63fa560b5379b9e7eb85c89e63d7e
amxr21 <ammar211080@gmail.com>
2025-04-23 07:02:46 +0400

    add developer special mark

--- frontend/src/components/Row.jsx
+++ frontend/src/components/Row.jsx
@@ -107,7 +107,7 @@ const Row = ({number, name, ticketId, uniId, email, phoneNumber, studyLevel, maj
     else{
         return userType != 'manager'
         ?
-            <div className={`row grid py-4 pl-7 pr-14 min-h-24 ${flags?.includes(user?.companyName) ? "border border-2 border-green-500 bg-white" :'bg-white'} rounded-xl items-center mb-2 text-sm xl:text-base`}>
+            <div className={`relative overflow-hidden row grid py-4 pl-7 pr-14 min-h-24 ${flags?.includes(user?.companyName) ? "border border-2 border-green-500 bg-white" :'bg-white'} rounded-xl items-center mb-2 text-sm xl:text-base`}>
                 <h2 className="flex">{number}
                     
                 </h2>
@@ -122,7 +122,25 @@ const Row = ({number, name, ticketId, uniId, email, phoneNumber, studyLevel, maj
                 </div>
                 <h2 className={`flex text-center justify-center text-sm xl:text-[1rem] px-2 py-2 rounded-xl font-semibold ${status ? `${colorCode.Confirmed}` : `${colorCode.Registered}`}`}>{status ? 'Confirmed' : 'Registered'}</h2>
     
-               
+                {
+                    uniId == 22105176
+                    ?
+                    <div className="bg-[#dba514] developer-mark flex items-center justify-start absolute origin-top-left h-full w-24 top-0 -right-14 hover:right-0 transition-all smooth ease-in-out z-50">
+                        <div className="flex flex-col p-3 gap-0 max-w-full text-brown-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-5 -mb-8 z-50">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+                            </svg>
+
+                            <p className="opacity-0 hover:opacity-100 transition-all smooth ease-in-out text-sm text-white font-medium w-full pt-10 ">
+                                The Developer
+                            </p> 
+
+                        </div>
+                            
+                    </div>
+                    :
+                    ""
+                }
                
                
                
