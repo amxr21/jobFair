cd8cc9ac97287f76140267c1b997ce48a68a3767
amxr21 <ammar211080@gmail.com>
2025-04-24 06:59:12 +0400

    Create DeveloperBadge.jsx

---
+++ frontend/src/components/DeveloperBadge.jsx
@@ -0,0 +1,19 @@
+const DeveloperBadge = () => {
+    return (
+        <div className="bg-[#dba514] developer-mark flex items-center justify-start absolute origin-top-left h-full w-24 top-0 -right-14 hover:right-0 transition-all smooth ease-in-out z-50">                    
+            <div className="flex flex-col p-3 gap-0 max-w-full">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-5 -mb-8 z-50">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+                </svg>
+
+                <p className="opacity-0 hover:opacity-100 transition-all smooth ease-in-out text-sm text-white font-medium w-full pt-10 ">
+                    The Developer
+                </p> 
+
+            </div>
+        </div>
+    )
+}
+
+
+export default DeveloperBadge
