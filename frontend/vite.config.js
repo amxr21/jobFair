d266c6e5e1129e5eb6f8b0d3538d81803f309067
amxr21 <ammar211080@gmail.com>
2025-05-27 22:13:51 +0400

    update css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     host: true,  // Allows network access to the development server
     port: 3000,  // Optional: specify a port; otherwise, Vite will pick one
