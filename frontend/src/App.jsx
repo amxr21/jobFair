7d7162be118dac42017a3002f71d73c16537c090
amxr21 <ammar211080@gmail.com>
2025-05-27 22:15:05 +0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
-import { NavBar, MainBanner } from "./components/index";
+ 
 
-import Managers from "./pages/Managers";
-import Statistics from "./pages/Statistics";
+import { Managers, Statistics, Survey, MainBanner, NavBar, SurveyResults } from "./pages/index";
 
 import { useAuthContext } from "./Hooks/useAuthContext";
 
@@ -19,7 +18,6 @@ const link = "https://jobfair-production.up.railway.app"
 
  
 function App() {
-
   
   const { user } = useAuthContext();
 
@@ -30,10 +28,12 @@ function App() {
     <div className="App grid grid-cols-12 relative gap-x-8 xl:gap-x-8 p-0 md:p-8 h-[100vh] overflow-hidden">
       {/* <Intro /> */}
       <BrowserRouter>
-      <NavBar/>
+      <NavBar link={link}/>
         <Routes>
           <Route path="/" element={<MainBanner link={link}/>} />
           <Route path="/managers" element={<Managers link={link} />} />
+          <Route path="/survey" element={<Survey/>} />
+          <Route path="/surveyResults" element={<SurveyResults/>} />
           <Route path="/statistics" element={<Statistics link={link} />} />
           <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
           <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
