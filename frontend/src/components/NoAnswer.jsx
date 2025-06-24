72d3a9a9ed647754940b8506484a9681b17f71d9
amxr21 <ammar211080@gmail.com>
2025-06-25 01:05:55 +0400

    Create NoAnswer.jsx

---
+++ frontend/src/components/NoAnswer.jsx
@@ -0,0 +1,9 @@
+const NoAnswer = () => {
+    return (
+        <div className="text-lg border-b w-full p-2 text-gray-300">
+            No Answer has been recorder
+        </div>
+    )
+}
+
+export default NoAnswer;
