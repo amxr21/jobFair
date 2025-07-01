35a3d8e85756dab7800c162ec4a0c7ea624e255e
amxr21 <ammar211080@gmail.com>
2025-07-01 14:40:58 +0400

    Update NoAnswer.jsx

--- frontend/src/components/NoAnswer.jsx
+++ frontend/src/components/NoAnswer.jsx
@@ -1,7 +1,7 @@
 const NoAnswer = () => {
     return (
         <div className="text-lg border-b w-full p-2 text-gray-300">
-            No Answer has been recorder
+            No Answer has been recorded
         </div>
     )
 }
